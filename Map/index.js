ac66ce6e766b5845cad8a087e93c7d575a7bf43b
Samandar0021305 <tsamandardev89@gmail.com>
2024-06-05 01:40:25 +0500

    fix

--- Map/index.js
+++ Map/index.js
@@ -1,7 +1,31 @@
-class Map {
+class MyMap {
     data;
     constructor(){
      this.data = {};
     };
-    
-}
+
+    set(key,value){
+       this.data[key] = value;
+    }
+    get(key){
+     return this.data[key];
+    };
+    has(key){
+        return `${key}` in this.data;
+    }
+    delete(key){
+      delete this.data[key]
+    };
+    clear(){
+        this.data = {};
+    };
+    size(){
+        return Object.keys(this.data).length;
+    }
+};
+
+
+const map = new MyMap();
+map.set(3,4)
+console.log(map.has(3))
+console.log(map.constructor)
