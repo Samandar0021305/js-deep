4b106d1c252b8121e6496dde9cadc1eecadffd92
Samandar0021305 <tsamandardev89@gmail.com>
2024-06-08 21:07:54 +0500

    number loadash method

--- loadash/number.js
+++ loadash/number.js
@@ -7,8 +7,23 @@ class NumberLoadash {
          if(item != a && item != b) return item;
       }
     }
+    inRange(x,y,z){
+       if( z == undefined && x < y && x > 0  || ( x > y && x < z )) return true;
+       return false
+    }
+    random(x,y){
+       if(x.constructor.name == y.constructor.name){
+           return Math.random()*Math.abs(x-y) + Math.min(x,y);
+       }else{
+         if(y.constructor.name != "Number"){
+           return Math.random()*x;
+         }
+         return Math.random();
+       }
+    }
 }
 
 const _ = new NumberLoadash();
 
 const res = _.clamp(-10, -5, 5);
+const res1 = _.inRange(3, 4)
